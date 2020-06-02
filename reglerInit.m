60e3902b002c94931ae9aa0d4200b272e1e98be8
jan <jan@hans.fritz.box>
2020-06-02 15:37:11 +0200

    erste anpassungen

--- reglerInit.m
+++ reglerInit.m
@@ -12,18 +12,26 @@ ml = ml;
 l = 1;
 L = 5;
 r = 0.041;
-k = 7.2e-3;
-g = 9.81; 
+kr = 7.2e-3;
+g = 9.81;
 alpha_max = pi/6;
 M_max = 0.2;
 w_max = 17.8;
 P_max = 3.56;
 u_max = 100;
 
+
 % 4.2.1 Zustandsraumdarstellung
 dz = [dphi; solve(eqns(1), ddphi); dx; solve(eqns(2), ddx)]
 
 
-% 4.2.2
+% 4.2.2 Beschränkungen
+x_max = L/2;
+phi_max = pi/4;
+
+% 4.2.3 relative Leistung bestimmen
+
+u = 
+
 
 
