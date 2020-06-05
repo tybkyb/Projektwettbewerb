14ab11c1bdef3621febb4bbccddef94b5ac16b7e
tybkyb <47321571+tybkyb@users.noreply.github.com>
2020-06-05 17:30:22 +0200

    zustandsrückführung

--- Aufgabe4/Aufgabe4.m
+++ Aufgabe4/Aufgabe4.m
@@ -19,12 +19,27 @@ b2 = [0,0,1,0,9.81];
 [A1, B1, C1, D1] = tf2ss(b1,a1);
 [A2, B2, C2, D2] = tf2ss(b2,a2);
 
+A = [0, 1, 0, 0;
+     -9.81*(1+ml/0.8), 0, 0, .009;
+     0, 0, 0, 1;
+     12.26*ml, 0, 0, -.009];
+B = [0, -30.49, 0, 30.49]';
+
+C = eye(4);
+D = [0;0;0;0];
+
 Kp = zpk(-26.2465990577772, -3429.01569082840, -26129.2191287719);
 Ks = zpk(-0.4, -11459, 286474.999999292);
 
-figure
-bode(Guy1)
-figure
-nyquist(Guy1)
+% figure
+% bode(Guy1)
+% figure
+% nyquist(Guy1)
 
+pole_zustand = [-12, -10, -5, -11];
+pole_beob = pole_zustand(4) * [4, 4.1, 4.2, 4.3];
 
+Kz = place(A, B, pole_zustand);
+Kb = place(A', C', pole_beob);
+
+V =  -1/(C(3,:)*inv(A-B*Kz)*B); 
--- reglerInit.m
+++ reglerInit.m
@@ -11,15 +11,23 @@ A = [0, 1, 0, 0;
      12.26*ml, 0, 0, -.009];
 B = [0, -30.49, 0, 30.49]';
 
-Guy1 = -s/(.041*(0.8*s^3 + 0.0072*s^2 + 9.81*(0.8 + ml)*s + 0.071));
-Guy2 = (s^2 + 9.81)/(.041*s*(0.8*s^3 + 0.0072*s^2 + 9.81*(0.8 + ml)*s + 0.071));
+C = eye(4);
+D = [0;0;0;0];
 
+Kp = zpk(-26.2465990577772, -3429.01569082840, -26129.2191287719);
+Ks = zpk(-0.4, -11459, 286474.999999292);
 
-load('K.mat');
+% figure
+% bode(Guy1)
+% figure
+% nyquist(Guy1)
 
-figure
-bode(Kp*Guy1)
-figure
-nyquist(Kp*Guy1)
+pole_zustand = [-20, -1, -10, -2];
+pole_beob = pole_zustand(4) * [4, 4.1, 4.2, 4.3];
+
+Kz = place(A, B, pole_zustand);
+Kb = place(A', C', pole_beob);
+
+V =  -1/(C(3,:)*inv(A-B*Kz)*B); 
 
 
